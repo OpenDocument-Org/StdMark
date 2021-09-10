a9b937e6d56973dc97d69243e20b73b2fe1ab6db
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-09-10 14:25:02 +0900

    ud: update publish-bat

--- tool/publish.bat
+++ tool/publish.bat
@@ -1,11 +1,15 @@
 @echo off
-cd .\impl\Dart\
-rem cd .\CUMO_Transer
-rem pub publish
-rem cd ..\
-rem cd .\UMD_Documenter
-rem pub publish
-rem cd ..\
+cd %~dp0
+cd .\..\impl\Dart
+echo Dart impl
+if %1==cumo (
+cd .\CUMO_Transer
+pub publish
+) else if %1==doc (
+cd .\UMD_Documenter
+pub publish
+) else if %1==tools (
 cd .\UMD_Tools
 pub publish
-cd .\..\..\..\
+)
+cd %~dp0
